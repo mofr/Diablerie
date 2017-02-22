97ad29a849b2eaaceffeb59e81e559fa50e0a19d
mofr <mofr@zond.org>
2017-02-23 02:41:33 +0700

    Add DCC frames reconstruction draft

--- Assets/Scripts/d2/DCC.cs
+++ Assets/Scripts/d2/DCC.cs
@@ -49,6 +49,8 @@ public class DCC
         public IntRect box = IntRect.zero;
         public Frame[] frames;
         public byte[] pixel_values = new byte[256];
+        public PixelBufferEntry[] pixelBuffer;
+        public int pb_nb_entry;
     }
 
     struct FrameBuffer
@@ -73,6 +75,11 @@ public class DCC
         public Cell[] cells;
         public int nb_cell_w = 0;
         public int nb_cell_h = 0;
+
+        public Texture2D texture;
+        public Color32[] texturePixels;
+        public int textureX;
+        public int textureY;
     }
 
     struct PixelBufferEntry
@@ -297,15 +304,15 @@ public class DCC
         }
     }
 
-    static void FillPixelBuffer(Header header, Direction dir, Streams streams)
+    static FrameBuffer FillPixelBuffer(Header header, Direction dir, Streams streams)
     {
         FrameBuffer frameBuffer = CreateFrameBuffer(dir); // dcc_prepare_buffer_cells
         Debug.Log("cells " + frameBuffer.nb_cell_w + "x" + frameBuffer.nb_cell_h);
 
-        PixelBufferEntry[] pixelBuffer = new PixelBufferEntry[DCC_MAX_PB_ENTRY];
-        for(int i = 0; i < pixelBuffer.Length; ++i)
+        dir.pixelBuffer = new PixelBufferEntry[DCC_MAX_PB_ENTRY];
+        for(int i = 0; i < dir.pixelBuffer.Length; ++i)
         {
-            pixelBuffer[i].val = new byte[4];
+            dir.pixelBuffer[i].val = new byte[4];
         }
         PixelBufferEntry[] cellBuffer = new PixelBufferEntry[frameBuffer.cells.Length];
         int pixelMask = 0;
@@ -386,7 +393,7 @@ public class DCC
                         PixelBufferEntry old_entry = cellBuffer[curr_cell];
                         pb_idx++;
                         Debug.Assert(pb_idx < DCC_MAX_PB_ENTRY);
-                        var newEntry = pixelBuffer[pb_idx];
+                        var newEntry = dir.pixelBuffer[pb_idx];
                         int curr_idx = decoded_pix - 1;
 
                         for (int i = 0; i < 4; i++)
@@ -403,10 +410,137 @@ public class DCC
                         }
                         newEntry.frame = f;
                         newEntry.frameCellIndex = x + (y * frame.nb_cell_w);
-                        pixelBuffer[pb_idx] = newEntry;
+                        dir.pixelBuffer[pb_idx] = newEntry;
                         cellBuffer[curr_cell] = newEntry;
                     }
                 }
+            }
+        }
+
+        for (int i = 0; i <= pb_idx; i++)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                int y = dir.pixelBuffer[i].val[x];
+                dir.pixelBuffer[i].val[x] = dir.pixel_values[y];
+            }
+        }
+
+        dir.pb_nb_entry = pb_idx + 1;
+
+        return frameBuffer;
+    }
+
+    static void MakeFrames(Header header, Direction dir, FrameBuffer frameBuffer, Streams streams)
+    {
+        for (int c = 0; c < frameBuffer.cells.Length; c++)
+        {
+            frameBuffer.cells[c].last_w = -1;
+            frameBuffer.cells[c].last_h = -1;
+        }
+
+        int pb_idx = 0;
+
+        for (int f = 0; f < header.framesPerDir; f++)
+        {
+            Frame frame = dir.frames[f];
+            int nb_cell = frame.nb_cell_w * frame.nb_cell_h;
+
+            for (int c = 0; c < nb_cell; c++)
+            {
+                Cell cell = frame.cells[c];
+
+                // buffer cell
+                int cell_x = cell.x0 / 4;
+                int cell_y = cell.y0 / 4;
+                int cell_idx = cell_x + (cell_y * frameBuffer.nb_cell_w);
+                Cell buff_cell = frameBuffer.cells[cell_idx];
+                PixelBufferEntry pbe = dir.pixelBuffer[pb_idx];
+
+                // equal cell checks
+                if ((pbe.frame != f) || (pbe.frameCellIndex != c))
+                {
+                    // this buffer cell have an equalcell bit set to 1
+                    //    so either copy the frame cell or clear it
+
+                    if ((cell.w != buff_cell.last_w) || (cell.h != buff_cell.last_h))
+                    {
+                        // different sizes
+                        //clear(cell->bmp); // set all pixels of the frame cell to 0
+                    }
+                    else
+                    {
+                        // same sizes
+
+                        // copy the old frame cell into its new position
+                        //blit(dir->bmp, dir->bmp,
+                        //     buff_cell->last_x0, buff_cell->last_y0,
+                        //     cell->x0, cell->y0,
+                        //     cell->w, cell->h
+                        //);
+
+                        // copy it again, into the final frame bitmap
+                        //blit(cell->bmp, frm_bmp,
+                        //     0, 0,
+                        //     cell->x0, cell->y0,
+                        //     cell->w, cell->h
+                        //);
+                    }
+                }
+                else
+                {
+                    // fill the frame cell with pixels
+
+                    if (pbe.val[0] == pbe.val[1])
+                    {
+                        // fill FRAME cell to color val[0]
+                        //clear_to_color(cell->bmp, pbe->val[0]);
+                    }
+                    else
+                    {
+                        int nb_bit;
+                        if (pbe.val[1] == pbe.val[2])
+                            nb_bit = 1;
+                        else
+                            nb_bit = 2;
+
+                        // fill FRAME cell with pixels
+                        for (int y = 0; y < cell.h; y++)
+                        {
+                            for (int x = 0; x < cell.w; x++)
+                            {
+                                int pix = streams.pixelCode.ReadBits(nb_bit);
+                                Color32 color = Palette.palette[pbe.val[pix]];
+                                int textureY = frame.textureY + dir.box.height - cell.y0 + cell.h - y;
+                                int textureX = frame.textureX + cell.x0 + x;
+                                frame.texturePixels[frame.texture.width * textureY + textureX] = color;
+                                //putpixel(cell->bmp, x, y, pbe->val[pix]);
+                            }
+                        }
+                    }
+
+                    // copy the frame cell into the frame bitmap
+                    //blit(cell->bmp, frm_bmp,
+                    //     0, 0,
+                    //     cell->x0, cell->y0,
+                    //     cell->w, cell->h
+                    //);
+
+                    // next pixelbuffer entry
+                    pb_idx++;
+                }
+
+                // for the buffer cell that was used by this frame cell,
+                // save the width & size of the current frame cell
+                // (needed for further tests about equalcell)
+                buff_cell.last_w = cell.w;
+                buff_cell.last_h = cell.h;
+
+                // and save its origin, for further copy when equalcell
+                buff_cell.last_x0 = cell.x0;
+                buff_cell.last_y0 = cell.y0;
+
+                frameBuffer.cells[cell_idx] = buff_cell;
             }
         }
     }
@@ -475,9 +609,17 @@ public class DCC
                     dir.box.xMax = Mathf.Max(dir.box.xMax, frame.box.xMax);
                     dir.box.yMax = Mathf.Max(dir.box.yMax, frame.box.yMax);
                 }
+            }
+
+            for (int f = 0; f < header.framesPerDir; ++f)
+            {
+                Frame frame = dir.frames[f];
+
+                int w = dir.box.width;
+                int h = dir.box.height;
 
                 int padding = 2;
-                var pack = packer.put(frame.width + padding, frame.height + padding);
+                var pack = packer.put(w + padding, h + padding);
                 if (pack.newTexture)
                 {
                     if (texture != null)
@@ -491,28 +633,32 @@ public class DCC
                 }
 
                 // debug frame
-                int debugCornerWidth = Mathf.Min(10, frame.width);
-                int debugCornerHeight = Mathf.Min(10, frame.height);
+                int debugCornerWidth = Mathf.Min(10, w);
+                int debugCornerHeight = Mathf.Min(10, h);
                 for (int i = 0; i < debugCornerWidth; ++i)
-                    pixels[textureSize * (pack.y + frame.height) + pack.x + i] = Color.red;
+                    pixels[textureSize * (pack.y + h) + pack.x + i] = Color.red;
                 for (int i = 0; i < debugCornerWidth; ++i)
-                    pixels[textureSize * (pack.y + frame.height - 1) + pack.x + i] = Color.red;
+                    pixels[textureSize * (pack.y + h - 1) + pack.x + i] = Color.red;
                 for (int i = 0; i < debugCornerHeight; ++i)
-                    pixels[textureSize * (pack.y + frame.height - i) + pack.x] = Color.red;
+                    pixels[textureSize * (pack.y + h - i) + pack.x] = Color.red;
                 for (int i = 0; i < debugCornerHeight; ++i)
-                    pixels[textureSize * (pack.y + frame.height - i) + pack.x + 1] = Color.red;
+                    pixels[textureSize * (pack.y + h - i) + pack.x + 1] = Color.red;
                 for (int i = 0; i < debugCornerWidth; ++i)
-                    pixels[textureSize * pack.y + pack.x - i + frame.width] = Color.blue;
+                    pixels[textureSize * pack.y + pack.x - i + w] = Color.blue;
                 for (int i = 0; i < debugCornerHeight; ++i)
-                    pixels[textureSize * (pack.y + i) + pack.x + frame.width] = Color.blue;
+                    pixels[textureSize * (pack.y + i) + pack.x + w] = Color.blue;
                 for (int i = 0; i < debugCornerWidth; ++i)
-                    pixels[textureSize * (pack.y + 1) + pack.x - i + frame.width] = Color.blue;
+                    pixels[textureSize * (pack.y + 1) + pack.x - i + w] = Color.blue;
                 for (int i = 0; i < debugCornerHeight; ++i)
-                    pixels[textureSize * (pack.y + i) + pack.x + frame.width - 1] = Color.blue;
+                    pixels[textureSize * (pack.y + i) + pack.x + w - 1] = Color.blue;
 
-                var spriteRect = new Rect(pack.x, pack.y, frame.width, frame.height);
-                var pivot = new Vector2(-frame.xoffset / (float)frame.width, frame.yoffset / (float)frame.height);
-                Sprite sprite = Sprite.Create(texture, spriteRect, pivot, Iso.pixelsPerUnit);
+                frame.texture = texture;
+                frame.texturePixels = pixels;
+                frame.textureX = pack.x;
+                frame.textureY = pack.y;
+                var textureRect = new Rect(pack.x, pack.y, w, h);
+                var pivot = new Vector2(-frame.xoffset / (float)w, frame.yoffset / (float)h);
+                Sprite sprite = Sprite.Create(texture, textureRect, pivot, Iso.pixelsPerUnit);
                 sprites.Add(sprite);
             }
 
@@ -525,7 +671,8 @@ public class DCC
 
             Debug.Log("box " + dir.box.AsString());
 
-            FillPixelBuffer(header, dir, streams); // dcc_fill_pixel_buffer
+            var frameBuffer = FillPixelBuffer(header, dir, streams); // dcc_fill_pixel_buffer
+            MakeFrames(header, dir, frameBuffer, streams); // dcc_make_frames
         }
 
         if (texture != null)
