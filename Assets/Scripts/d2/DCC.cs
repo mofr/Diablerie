5a176c1d1b48cef47876168893ee2cff742d2da8
mofr <mofr@zond.org>
2017-04-21 18:17:04 +0700

    Enable 32-direction DCC

--- Assets/Scripts/d2/DCC.cs
+++ Assets/Scripts/d2/DCC.cs
@@ -540,6 +540,7 @@ public class DCC
     readonly static int[] dirs4 = new int[] { 0, 1, 2, 3 };
     readonly static int[] dirs8 = new int[] { 4, 0, 5, 1, 6, 2, 7, 3 };
     readonly static int[] dirs16 = new int[] { 4, 8, 0, 9, 5, 10, 1, 11, 6, 12, 2, 13, 7, 14, 3, 15 };
+    readonly static int[] dirs32 = new int[] { 4, 16, 8, 17, 0, 18, 9, 19, 5, 20, 10, 21, 1, 22, 11, 23, 6, 24, 12, 25, 2, 26, 13, 27, 7, 28, 14, 29, 3, 30, 15, 31 };
 
     public List<Sprite> GetSprites(int d)
     {
@@ -559,6 +560,10 @@ public class DCC
             case 4: dirs = dirs4; break;
             case 8: dirs = dirs8; break;
             case 16: dirs = dirs16; break;
+            case 32: dirs = dirs32; break;
+            default:
+                Debug.LogError("Invalid DCC direction count: " + header.directionCount);
+                return;
         }
 
         var bitReader = new BitReader(bytes, header.dirOffset[dirs[d]] * 8);
