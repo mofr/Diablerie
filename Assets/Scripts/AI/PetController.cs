7d8c602eccb38ff118917d99508e623adf0145f8
mofr <mofr@zond.org>
2019-11-10 01:13:55 +0700

    Make pets attacking enemies

--- Assets/Scripts/AI/PetController.cs
+++ Assets/Scripts/AI/PetController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class PetController : MonoBehaviour
 {
     public Character owner;
-    public float roamRadius = 25f;
+    public float roamRadius = 15f;
+    public float agroRadius = 3f;
+    public float agroOwnerRadius = 5f;
     private Character character;
     private Character target;
 
@@ -24,10 +26,42 @@ public class PetController : MonoBehaviour
         {
             if (owner == null)
                 yield return null;
+            Character nearestEnemy = AIUtils.GetNearestEnemy(character, agroRadius);
+            if (nearestEnemy == null)
+                nearestEnemy = AIUtils.GetNearestEnemy(owner, agroOwnerRadius);
+            if (nearestEnemy != null)
+            {
+                Attack(nearestEnemy);
+                yield break;
+            }
             var newPosition = owner.iso.pos + new Vector2(Random.Range(-roamRadius, roamRadius), Random.Range(-roamRadius, roamRadius));
             character.GoTo(newPosition);
             yield return new WaitForSeconds(Random.Range(1f, 2f));
             while (!isActiveAndEnabled) yield return null;
+        }
+    }
+    
+    private void Attack(Character target)
+    {
+        this.target = target;
+        StartCoroutine(Attack());
+    }
+
+    private IEnumerator Attack()
+    {
+        while (true)
+        {
+            if (AIUtils.IsAttackable(character, target))
+            {
+                character.UseSkill(SkillInfo.Attack, target);
+                yield return new WaitForSeconds(0.1f);
+            }
+            else
+            {
+                target = null;
+                StartCoroutine(Roam());
+                yield break;
+            }
         }
     }
 }
