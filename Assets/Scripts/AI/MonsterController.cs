c7d6ad31e21e4178a00699f4f83e3801669b1680
mofr <mofr@zond.org>
2019-11-09 23:40:42 +0700

    Extract AIUtils from MonsterController

--- Assets/Scripts/AI/MonsterController.cs
+++ Assets/Scripts/AI/MonsterController.cs
@@ -10,8 +10,6 @@ public class MonsterController : MonoBehaviour
     readonly float maxAgroDistance = 15f;
     bool tauntSaid = false;
 
-    static Collider2D[] visibleColliders = new Collider2D[100];
-
 	void Awake()
     {
         iso = GetComponent<Iso>();
@@ -25,48 +23,40 @@ public class MonsterController : MonoBehaviour
 
     private IEnumerator Roam()
     {
-        while (!this.target)
+        while (!target)
         {
-            int visibleCount = Physics2D.OverlapCircleNonAlloc(transform.position, viewRadius, visibleColliders);
-            for (int i = 0; i < visibleCount; ++i)
+            Character nearestEnemy = AIUtils.GetNearestEnemy(character, viewRadius);
+            if (nearestEnemy != null)
             {
-                var collider = visibleColliders[i];
-                var visibleCharacter = collider.GetComponent<Character>();
-                if (visibleCharacter == null)
-                    continue;
-                if (IsAttackable(visibleCharacter))
-                {
-                    if (!tauntSaid)
-                    {
-                        AudioManager.instance.Play(character.monStat.sound.taunt, character.transform);
-                        tauntSaid = true;
-                    }
-                    Attack(visibleCharacter);
-                    yield break;
-                }
+                Attack(nearestEnemy);
+                yield break;
             }
 
-            var target = iso.pos + new Vector2(Random.Range(-8f, 8f), Random.Range(-8f, 8f));
-            character.GoTo(target);
-            yield return new WaitForSeconds(Random.Range(1f, 3f));
+            var newPosition = iso.pos + new Vector2(Random.Range(-8f, 8f), Random.Range(-8f, 8f));
+            character.GoTo(newPosition);
+            yield return new WaitForSeconds(Random.Range(1f, 2f));
             while (!isActiveAndEnabled) yield return null;
         }
+    }
+
+    private void Attack(Character target)
+    {
+        if (!tauntSaid)
+        {
+            AudioManager.instance.Play(character.monStat.sound.taunt, character.transform);
+            tauntSaid = true;
+        }
+
+        this.target = target;
+        StartCoroutine(Attack());
     }
 
     private bool IsAttackable(Character target)
     {
         bool targetTooFar = Vector2.Distance(target.iso.pos, iso.pos) > maxAgroDistance;
-        bool targetDead = target.Mode == "DT" || target.Mode == "DD";
-        bool allied = target.party == character.party;
-        return !targetTooFar && !targetDead && !allied;
+        return !targetTooFar && AIUtils.IsAttackable(character, target);
     }
     
-    private void Attack(Character target)
-    {
-        this.target = target;
-        StartCoroutine(Attack());
-    }
-
     private IEnumerator Attack()
     {
         while (true)
