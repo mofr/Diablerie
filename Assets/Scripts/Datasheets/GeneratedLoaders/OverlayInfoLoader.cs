24807ed482a52a3a2a9308891d01afab3782f6e5
mofr <mofr@zond.org>
2019-11-24 18:27:19 +0700

    Upgrade DatasheetLoaderGenerator for arrays of primitives

--- Assets/Scripts/Datasheets/GeneratedLoaders/OverlayInfoLoader.cs
+++ Assets/Scripts/Datasheets/GeneratedLoaders/OverlayInfoLoader.cs
@@ -5,47 +5,33 @@ class OverlayInfoLoader : Datasheet.Loader<OverlayInfo>
     public void LoadRecord(ref OverlayInfo record, string[] values)
     {
         int index = 0;
-            Datasheet.Parse(values[index], ref record.id);
-            index++;
-            Datasheet.Parse(values[index], ref record.filename);
-            index++;
+            Datasheet.Parse(values[index++], ref record.id);
+            Datasheet.Parse(values[index++], ref record.filename);
             record.unused = new string[3];
-            index += 3;  // TODO implement arrays
-            Datasheet.Parse(values[index], ref record.preDraw);
-            index++;
+            Datasheet.Parse(values[index++], ref record.unused[0]);
+            Datasheet.Parse(values[index++], ref record.unused[1]);
+            Datasheet.Parse(values[index++], ref record.unused[2]);
+            Datasheet.Parse(values[index++], ref record.preDraw);
             record.unused2 = new string[4];
-            index += 4;  // TODO implement arrays
-            Datasheet.Parse(values[index], ref record.xOffset);
-            index++;
-            Datasheet.Parse(values[index], ref record.yOffset);
-            index++;
-            Datasheet.Parse(values[index], ref record.height1);
-            index++;
-            Datasheet.Parse(values[index], ref record.height2);
-            index++;
-            Datasheet.Parse(values[index], ref record.height3);
-            index++;
-            Datasheet.Parse(values[index], ref record.height4);
-            index++;
-            Datasheet.Parse(values[index], ref record.fps);
-            index++;
-            Datasheet.Parse(values[index], ref record.loopWaitTime);
-            index++;
-            Datasheet.Parse(values[index], ref record.trans);
-            index++;
-            Datasheet.Parse(values[index], ref record.initRadius);
-            index++;
-            Datasheet.Parse(values[index], ref record.radius);
-            index++;
-            Datasheet.Parse(values[index], ref record.red);
-            index++;
-            Datasheet.Parse(values[index], ref record.green);
-            index++;
-            Datasheet.Parse(values[index], ref record.blue);
-            index++;
-            Datasheet.Parse(values[index], ref record.numDirections);
-            index++;
-            Datasheet.Parse(values[index], ref record.localBlood);
-            index++;
+            Datasheet.Parse(values[index++], ref record.unused2[0]);
+            Datasheet.Parse(values[index++], ref record.unused2[1]);
+            Datasheet.Parse(values[index++], ref record.unused2[2]);
+            Datasheet.Parse(values[index++], ref record.unused2[3]);
+            Datasheet.Parse(values[index++], ref record.xOffset);
+            Datasheet.Parse(values[index++], ref record.yOffset);
+            Datasheet.Parse(values[index++], ref record.height1);
+            Datasheet.Parse(values[index++], ref record.height2);
+            Datasheet.Parse(values[index++], ref record.height3);
+            Datasheet.Parse(values[index++], ref record.height4);
+            Datasheet.Parse(values[index++], ref record.fps);
+            Datasheet.Parse(values[index++], ref record.loopWaitTime);
+            Datasheet.Parse(values[index++], ref record.trans);
+            Datasheet.Parse(values[index++], ref record.initRadius);
+            Datasheet.Parse(values[index++], ref record.radius);
+            Datasheet.Parse(values[index++], ref record.red);
+            Datasheet.Parse(values[index++], ref record.green);
+            Datasheet.Parse(values[index++], ref record.blue);
+            Datasheet.Parse(values[index++], ref record.numDirections);
+            Datasheet.Parse(values[index++], ref record.localBlood);
     }
 }
